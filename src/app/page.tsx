df924492bc049c85afcea6636ff4d7e5eb2c6a64
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-03-07 20:59:55 -0500

    Add link to shop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,6 +14,16 @@ export default function Home() {
       <h1 className="text-center font-bold text-lg text-zinc-700 sm:text-3xl">
         New website coming soon!
       </h1>
+
+      <p className="text-center text-base text-zinc-500 sm:text-lg">
+        In the meantime, visit our shop at{" "}
+        <Link
+          href="https://shop.freemadeira.org"
+          className="font-bold text-yellow-900"
+        >
+          shop.freemadeira.org
+        </Link>
+      </p>
     </div>
   );
 }
