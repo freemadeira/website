a5f518331ae3631eb6da9fd8a905dc7488601c52
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-03-11 00:45:09 -0500

    Change layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Schibsted_Grotesk, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Footer } from '@/components/footer';
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: '--font-main',
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang='en' className={`${schibstedGrotesk.variable}`}>
       <body className='min-h-dvh antialiased'>{children}</body>
+        <Footer />
     </html>
   );
 }
