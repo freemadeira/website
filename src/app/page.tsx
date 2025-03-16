522942582c430b9cd9065e57da39711d0b5f06b7
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-03-15 23:57:20 -0500

    Add components to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
+import { UpcomingEvents } from '@/components/events';
+import { Education, Merchants } from '@/components/home';
+import { Hero } from '@/components/home/Hero';
+import { Container } from '@/components/ui/atoms';
+import { PortalHands } from '@/components/ui/svgs';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="mx-16 flex h-full flex-col items-center justify-center gap-10">
-      <Image src="/free-madeira-logo.svg" alt="Vercel Logo" width={400} height={400} />
+    <>
+      <Hero />
 
-      <h1 className="text-center font-bold text-lg text-zinc-700 sm:text-3xl">
-        New website coming soon!
-      </h1>
+      <UpcomingEvents />
 
-      <p className="text-center text-base text-zinc-500 sm:text-lg">
-        In the meantime, visit our shop at{' '}
-        <Link href="https://shop.freemadeira.org" className="font-bold text-yellow-900">
-          shop.freemadeira.org
-        </Link>
-      </p>
-    </div>
+      <Education />
+
+      <Merchants />
+    </>
   );
 }
