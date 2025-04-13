779107b5105bcfd3792dc568d7dadebc5cb4f0cb
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-04-13 09:34:13 -0600

    Add header to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Schibsted_Grotesk } from 'next/font/google';
 import './globals.css';
 import { Footer } from '@/components/footer';
+import { Header } from '@/components/header';
+// import { useTheme } from '@/hooks/useTheme';
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: '--font-main',
@@ -18,10 +20,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // useTheme();
+
   return (
     <html lang="en" className={`${schibstedGrotesk.variable}`}>
       <body className="flex min-h-dvh flex-col antialiased">
+        <Header />
+
         <main className="grow">{children}</main>
+
         <Footer />
       </body>
     </html>
