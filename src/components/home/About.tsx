fbe4581bb1f823ab942fd5f9062f298f6a510121
Henrique <h@Henriques-MacBook-Air.local>
2025-04-21 01:30:12 -0700

    Fix spacing

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -1,4 +1,4 @@
-import { CategoryTag, Container, Flex, Heading } from '../ui/atoms';
+import { Button, CategoryTag, Container, Flex, Heading } from '../ui/atoms';
 import { HandCoin } from '../ui/svgs/HandCoin';
 
 export function About(): React.ReactElement {
@@ -7,12 +7,17 @@ export function About(): React.ReactElement {
       <CategoryTag>About us</CategoryTag>
 
       <Flex className="flex-col-reverse sm:flex-row sm:gap-48" alignItems="end">
-        <Flex direction="column" className="w-full gap-8 sm:w-4/7 sm:gap-30">
-          <Heading size="h3">
-            We support the local community and position Madeira as a technology leader in Bitcoin
-            adoption.
-          </Heading>
-          Discreet button
+        <Flex direction="column" gap={12} className="h-full w-full sm:w-4/7" justifyContent="end">
+          <div className="grow">
+            <Heading size="h3" className="grow">
+              We support the local community and position Madeira as a technology leader in Bitcoin
+              adoption.
+            </Heading>
+          </div>
+
+          <Button as="a" href="mailto:info@freemadeira.org" fill="outlined">
+            Partnerships
+          </Button>
         </Flex>
 
         <HandCoin className="mt-14 mb-10 w-full sm:my-0 sm:w-3/7" />
