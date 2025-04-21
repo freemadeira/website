23ed60aaf4cc413e2a79342b14b08608dceb83d7
Henrique <h@Henriques-MacBook-Air.local>
2025-04-21 00:50:40 -0700

    Update footer links

--- src/components/footer/Links.tsx
+++ src/components/footer/Links.tsx
@@ -1,35 +1,29 @@
 import { Flex, Grid } from '@/components/ui/atoms';
+import Link from 'next/link';
 
 export function Links(): React.ReactElement {
   return (
     // TODO: Fix when we can use responsive variants
-    <Grid cols={2} gap={8} className="grow sm:grid-cols-4">
+    <Grid cols={2} gap={8} className="grow text-sm leading-[200%] sm:grid-cols-4">
+      {/* <Flex direction="column"></Flex> */}
+
+      {/* <Flex direction="column"></Flex> */}
+
       <Flex direction="column">
-        <p>Links</p>
-        <p>Links</p>
-        <p>Links</p>
-        <p>Links</p>
+        <Link
+          href="https://shop.freemadeira.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-bold"
+        >
+          Shop
+        </Link>
       </Flex>
 
       <Flex direction="column">
-        <p>Links</p>
-        <p>Links</p>
-        <p>Links</p>
-        <p>Links</p>
-      </Flex>
-
-      <Flex direction="column">
-        <p>Links</p>
-        <p>Links</p>
-        <p>Links</p>
-        <p>Links</p>
-      </Flex>
-
-      <Flex direction="column">
-        <p>Links</p>
-        <p>Links</p>
-        <p>Links</p>
-        <p>Links</p>
+        <Link href="https://bitcoin.org/bitcoin.pdf" target="_blank" rel="noopener noreferrer">
+          Bitcoin Whitepaper
+        </Link>
       </Flex>
     </Grid>
   );
