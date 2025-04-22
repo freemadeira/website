068d4de587335836f5f1fad7ecdfd27e4f697181
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-04-21 20:28:33 -0700

    Remove comment
    Small change to trigger redeploy

--- src/components/footer/Links.tsx
+++ src/components/footer/Links.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 
 export function Links(): React.ReactElement {
   return (
-    // TODO: Fix when we can use responsive variants
     <Grid cols={2} gap={8} className="grow text-sm leading-[200%] sm:grid-cols-4">
       {/* <Flex direction="column" /> */}
 
