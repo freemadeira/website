c6a2c84026ae8a6fb2929ab8ec36387c5994127c
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-05-18 22:48:18 +1000

    Small changes in home components

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -2,6 +2,5 @@ export * from './About';
 export * from './Carousel';
 export * from './Contribute';
 export * from './Education';
-export * from './Hero';
 export * from './Merchants';
 export * from './Partners';
