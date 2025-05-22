16cebd8df13f2c4421a820cfa49581cf42880ed1
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-05-22 16:12:19 +1000

    Add About page link in footer

--- src/components/footer/Links.tsx
+++ src/components/footer/Links.tsx
@@ -4,20 +4,22 @@ export function Links(): React.ReactElement {
   return (
     <Grid cols={2} gap={8} className="grow text-sm leading-[200%] sm:grid-cols-4">
       {/* TODO: Add links to the footer as pages are created */}
+      <Flex direction="column">
+        <Link href="/about" className="font-bold">
+          About
+        </Link>
+      </Flex>
+
       {/* <Flex direction="column" /> */}
 
-      <Flex direction="column" />
-
       <Flex direction="column">
-        <Link href="https://shop.freemadeira.org" className="font-bold" size="sm">
+        <Link href="https://shop.freemadeira.org" className="font-bold">
           Shop
         </Link>
       </Flex>
 
       <Flex direction="column">
-        <Link href="https://bitcoin.org/bitcoin.pdf" size="sm">
-          Bitcoin Whitepaper
-        </Link>
+        <Link href="https://bitcoin.org/bitcoin.pdf">Bitcoin Whitepaper</Link>
       </Flex>
     </Grid>
   );
