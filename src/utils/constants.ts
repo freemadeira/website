95417304036bf0710314250d0123825b179cbd76
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-05-24 00:51:28 +1000

    Add helper functions and move mailerlite constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,9 @@
 import type { Social } from '@/components/ui/atoms/SocialButton';
 
+const prod = { BASE_URL: 'https://freemadeira.org' };
+const dev = { BASE_URL: 'http://localhost:3000' };
 export const DEV_ENV = process.env.NODE_ENV === 'development';
-
-// These IDs are used to add subscribers to groups on MailerLite, based on subscription source and preferred language
-export const MAILERLITE_WEBSITE_GROUP_ID = '153157474249082044'; // Group ID for website subscribers
-export const MAILERLITE_PORTUGUESE_GROUP_ID = '153161515456792037'; // Group ID for Portuguese language
-export const MAILERLITE_ENGLISH_GROUP_ID = '153161521070868114'; // Group ID for English language
+export const CONFIG = DEV_ENV ? dev : prod;
 
 export const freeMadeiraSocials: Record<Social, string> = {
   facebook: 'freemadeira',
