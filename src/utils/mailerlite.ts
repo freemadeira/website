d0bc6c1bd34ee172d4c1533d1b0162208721de49
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-05-24 00:49:19 +1000

    Add helper functions

---
+++ src/utils/mailerlite.ts
@@ -0,0 +1,8 @@
+// The following are lookup objects used to get a language name from group ID and vice versa
+// export const GROUP_ID: Record<Language, string> = Object.fromEntries(
+//   LANGUAGE_GROUPS.map(({ language, groupId }) => [language, groupId]),
+// ) as Record<Language, string>;
+//
+// export const LANGUAGE: Record<string, Language> = Object.fromEntries(
+//   LANGUAGE_GROUPS.map(({ language, groupId }) => [groupId, language]),
+// ) as Record<string, Language>;
