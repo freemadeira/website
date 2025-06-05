f0659ec17071b56ed08d16882213c282ada4657c
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-06-05 14:28:18 +1000

    Update events

--- src/components/events/UpcomingEvents.tsx
+++ src/components/events/UpcomingEvents.tsx
@@ -12,22 +12,13 @@ export interface Event {
 
 const events: Event[] = [
   {
-    date: 'June 4, 2025, 18:30',
-    title: 'Monthly FREE Madeira Bitcoin Meetup',
-    description:
-      'Every first Wednesday of the month, FREE Madeira organises a Bitcoin meetup, bringing the community together. Know more and sign up',
-    tags: ['Meetup', 'In-person'],
-    imgSrc: '/images/events/meetup.jpg',
-    link: 'https://www.meetup.com/madeira-bitcoin-meetup-group/events/306809019/',
-  },
-  {
     date: 'June 21, 2025, 10:00',
     title: 'Introdução ao Bitcoin',
     description:
       'This introductory workshop explores how money evolved into fiat currencies, what the problems are with it and how Bitcoin can be the solution. By the end of it, you will also be able to use bitcoin for everyday savings and payments. This workshop is carried out in Portuguese.',
     tags: ['Workshop', 'Online', 'Education'],
     imgSrc: '/images/events/workshop-intro.jpg',
-    // TODO: Add link
+    link: 'https://forms.gle/umsn2vX1WSVk9d6RA',
   },
   {
     date: 'June 28, 2025, 10:00',
@@ -36,7 +27,7 @@ const events: Event[] = [
       'For merchants who want to learn about the potential of Bitcoin for their business and the customers they attract by accepting Bitcoin. This workshop will teach how to integrate bitcoin payments in your business and how to safely manage your funds. This workshop is carried out in Portuguese.',
     tags: ['Workshop', 'Online', 'Education'],
     imgSrc: '/images/events/workshop-merchants.jpg',
-    // TODO: Add link
+    link: 'https://forms.gle/RrmXK8sovXJppesk8',
   },
   {
     date: 'September–October (schedule TBC), 2025',
@@ -46,19 +37,19 @@ const events: Event[] = [
     tags: ['Course', 'Online'],
     imgSrc: '/images/events/diploma.png',
   },
-  // {
-  //   date: 'EXACT DATES TBC, 2028',
-  //   title: 'Bitcoin Atlantis 2028',
-  //   description:
-  //     'After the huge success of the first edition in 2024, Bitcoin Atlantis will return in 2028. We will once again celebrate Bitcoin in a halving year.',
-  //   tags: ['Conference'],
-  //   imgSrc: '/images/events/ba28.jpg',
-  // },
+  {
+    date: 'EXACT DATES TBC, 2028',
+    title: 'Bitcoin Atlantis 2028',
+    description:
+      'After the huge success of the first edition in 2024, Bitcoin Atlantis will return in 2028. We will once again celebrate Bitcoin in a halving year.',
+    tags: ['Conference'],
+    imgSrc: '/images/events/ba28.jpg',
+  },
 ];
 
 export function UpcomingEvents(): React.ReactElement {
   return (
-    <Container className='mx-0 mb-16 gap-7' outer>
+    <Container className="mx-0 mb-16 gap-7" outer>
       <Heading size="h5" className="mx-4 sm:mx-0">
         Upcoming Events
       </Heading>
