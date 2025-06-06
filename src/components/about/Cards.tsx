f1757accc5f404a69b18a156b3d7c77e37b11704
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-06-06 12:52:28 +1000

    Change Energy card

--- src/components/about/Cards.tsx
+++ src/components/about/Cards.tsx
@@ -5,9 +5,14 @@ import { Card } from './Card';
 export const Cards = () => {
   return (
     <Flex as={Container} direction="column" gap={7} className="my-20 sm:my-28 sm:flex-row sm:gap-9">
-      <Card icon={Zap} title="Energy">
+      <Card
+        icon={Zap}
+        title="Energy"
+        buttonHref="https://energysummit.freemadeira.org"
+        buttonText="FREE Energy Summit"
+      >
         <Text size="lg">
-          We support Bitcoin projects for energy sustainability in Madeira, focusing on education
+          We support Bitcoin projects for energy sustainability in Portugal, focusing on education
           and mentorship to foster local initiatives and expand renewable energy use.
         </Text>
       </Card>
