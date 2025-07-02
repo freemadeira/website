872445419c14c44ff754f6567659861baa0b3604
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-07-02 15:24:43 +1000

    Fix Contribute component

--- src/components/home/Contribute.tsx
+++ src/components/home/Contribute.tsx
@@ -7,18 +7,19 @@ export function Contribute(): React.ReactElement {
     <Background color="bridal">
       <Flex
         alignItems="center"
-        className="gap-16 pt-12 sm:flex-row sm:gap-44 sm:px-42 sm:pt-0"
+        className="gap-16 pt-12 md:max-w-2xl md:flex-row md:pt-0 lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl"
         as={Container}
         direction="column"
+        justifyContent="between"
       >
-        <Flex direction="column" className="w-full gap-8 sm:w-3/7 sm:gap-14">
+        <Flex direction="column" className="w-full gap-8 md:w-3/7 md:gap-14" alignItems="start">
           <Heading size="h4">Do you want to be part of the change?</Heading>
 
           <ContributeButtons />
         </Flex>
 
-        <Flex justifyContent="center" grow className="sm:pt-10">
-          <FingerTouch className="mx-8 w-full sm:h-[357px]" />
+        <Flex justifyContent="center" grow className="md:pt-10">
+          <FingerTouch className="mx-8 w-full md:h-[357px]" />
         </Flex>
       </Flex>
     </Background>
