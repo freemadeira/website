1d948b4f4aece5abacbe224288704e16b326051c
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-07-03 18:54:31 +1000

    Change icon

--- src/components/events/EventImage.tsx
+++ src/components/events/EventImage.tsx
@@ -1,5 +1,5 @@
 import { externalUrl } from '@/utils/functions';
-import { ArrowRight, ExternalLink } from 'lucide-react';
+import { ArrowRight, ArrowUpRight } from 'lucide-react';
 import Image from 'next/image';
 import { IconButton } from '../ui/atoms';
 import type { Event } from './UpcomingEvents';
@@ -24,7 +24,7 @@ export const EventImage: React.FC<EventImageProps> = ({ imgSrc, link }) => (
         <div className="size-full bg-gradient-to-tr from-transparent via-transparent to-black/60" />
 
         <IconButton className="absolute top-4 right-3" colour="white">
-          {externalUrl(link) ? <ExternalLink /> : <ArrowRight />}
+          {externalUrl(link) ? <ArrowUpRight /> : <ArrowRight />}
         </IconButton>
       </>
     )}
