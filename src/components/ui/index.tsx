c4154bc044fcfa97ada2540ec0df28582f0ca34f
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-07-07 20:37:42 +1000

    Add Breadcrumb component

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,5 +1,7 @@
+export * from './Breadcrumb';
 export * from './Carousel';
 export * from './ContributeButtons';
 export * from './Hero';
+export * from './Labels';
 export * from './SentenceList';
 export * from './SplitHighlight';
