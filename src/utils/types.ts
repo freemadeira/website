f7bca3d221c76a511a445e02f94cf73d639a9290
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-08-18 22:29:24 +0200

    Add types for table of contents

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,3 +5,25 @@ type UrlObject = NodeUrlObject & {
 };
 export type Url = string | UrlObject;
 export type hrefTarget = '_blank' | '_self';
+
+// Table of Contents types
+export type ContentsTable = {
+  parts: PartTOC[];
+};
+
+export type PartTOC = {
+  id: string;
+  title?: string;
+  chapters?: ChapterTOC[];
+};
+
+export type ChapterTOC = {
+  id: string;
+  title: string;
+  subjects?: SubjectTOC[];
+};
+
+export type SubjectTOC = {
+  id: string;
+  title: string;
+};
