3342233e71187077a8bc09e19576de822f10fca3
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2025-12-09 22:14:29 +0800

    Remove upcoming events section
    Temporarily make this section empty.

--- src/components/events/UpcomingEvents.tsx
+++ src/components/events/UpcomingEvents.tsx
@@ -49,19 +49,20 @@ const events: Event[] = [
 ];
 
 export function UpcomingEvents(): React.ReactElement {
-  return (
-    <Container wrapperClasses="mb-16 px-0" className="gap-7">
-      <Heading size="h5" className="mx-4 sm:mx-0">
-        Upcoming Events
-      </Heading>
-
-      <div className="no-scrollbar overflow-x-auto">
-        <Flex gap={8} className="w-fit overflow-x-auto px-4 sm:px-0">
-          {events.map((event) => (
-            <EventCard key={event.date + event.title} event={event} />
-          ))}
-        </Flex>
-      </div>
-    </Container>
-  );
+  return <></>;
+  // return (
+  //   <Container wrapperClasses="mb-16 px-0" className="gap-7">
+  //     <Heading size="h5" className="mx-4 sm:mx-0">
+  //       Upcoming Events
+  //     </Heading>
+  //
+  //     <div className="no-scrollbar overflow-x-auto">
+  //       <Flex gap={8} className="w-fit overflow-x-auto px-4 sm:px-0">
+  //         {events.map((event) => (
+  //           <EventCard key={event.date + event.title} event={event} />
+  //         ))}
+  //       </Flex>
+  //     </div>
+  //   </Container>
+  // );
 }
