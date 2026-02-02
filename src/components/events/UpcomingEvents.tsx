d992d9b809b30bb07eb585d4745f9b7ec240a168
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2026-02-02 10:18:22 +0000

    Update events

--- src/components/events/UpcomingEvents.tsx
+++ src/components/events/UpcomingEvents.tsx
@@ -13,23 +13,6 @@ export interface Event {
 
 const events: Event[] = [
   {
-    date: 'January 28, 2026, 18:30–20:00',
-    title: 'Introdução ao Bitcoin',
-    description:
-      'Workshop entirely in Portuguese. Este workshop apresenta Bitcoin como dinheiro. Exploramos a importância do dinheiro, a sua origem e evolução, e como Bitcoin representa o futuro do mesmo.',
-    imgSrc: '/images/events/workshop-intro.jpg',
-    tags: ['Workshop', 'Online'],
-  },
-  {
-    date: 'January 28, 2026, 18:30',
-    title: 'FREE Madeira Bitcoin Meetup',
-    description:
-      'In the month of January we welcome Sylvia Brinded-Püls, from A Place to Be, for an open and inspiring talk on sovereignty, community, and practical ways of living beyond the system.',
-    tags: ['Meetup', 'In-person'],
-    imgSrc: '/images/events/meetup.jpg',
-    href: 'https://www.meetup.com/madeira-bitcoin-meetup-group/events/312457728/',
-  },
-  {
     date: 'February 4, 2026, 18:30–20:00',
     title: 'Introdução ao lado técnico do Bitcoin',
     description:
@@ -46,14 +29,23 @@ const events: Event[] = [
     imgSrc: '/images/events/meetup.jpg',
     href: 'https://www.meetup.com/madeira-bitcoin-meetup-group/events/312459853/',
   },
-  // {
-  //   date: 'EXACT DATES TBC, 2028',
-  //   title: 'Bitcoin Atlantis 2028',
-  //   description:
-  //     'After the huge success of the first edition in 2024, Bitcoin Atlantis will return in 2028. We will once again celebrate Bitcoin in a halving year.',
-  //   tags: ['Conference'],
-  //   imgSrc: '/images/events/ba28.jpg',
-  // },
+  {
+    date: 'March 25, 2026, 18:30',
+    title: 'FREE Madeira Bitcoin Meetup',
+    description:
+      'Every last Wednesday of the month, FREE Madeira organises a Bitcoin meetup, bringing the community together. Know more and sign up.',
+    tags: ['Meetup', 'In-person'],
+    imgSrc: '/images/events/meetup.jpg',
+    href: 'https://www.meetup.com/madeira-bitcoin-meetup-group/events/312459864/',
+  },
+  {
+    date: 'EXACT DATES TBC, 2028',
+    title: 'Bitcoin Atlantis 2028',
+    description:
+      'After the huge success of the first edition in 2024, Bitcoin Atlantis will return in 2028. We will once again celebrate Bitcoin in a halving year.',
+    tags: ['Conference'],
+    imgSrc: '/images/events/ba28.jpg',
+  },
 ];
 
 export function UpcomingEvents(): React.ReactElement {
