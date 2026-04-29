856bb328b59a134d31426c4e34db126e7970e258
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2026-04-29 18:40:04 +0100

    Add event resource and controller

---
+++ api/app/resources/event_resource.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class EventResource
+  include Alba::Resource
+
+  attributes :id, :title, :description, :start_at, :end_at, :timezone,
+             :category, :language, :url, :image_url, :display_time
+
+  attribute :tags do |event|
+    event.tags.map(&:name)
+  end
+end
