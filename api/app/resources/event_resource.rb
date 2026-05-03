06b5d697a254640ab9a79c38c736857671960ab3
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2026-05-03 23:50:48 +0100

    Add comment

--- api/app/resources/event_resource.rb
+++ api/app/resources/event_resource.rb
@@ -6,5 +6,8 @@ class EventResource < ApplicationResource
   attributes :id, :title, :description, :start_at, :end_at, :timezone,
              :category, :language, :url, :image_url, :display_time
 
+  # TODO: image attachment. If there is an image attached, return the URL of the image. Otherwise,
+  # return the value of the `image_url` attribute.
+
   many :tags, resource: TagResource
 end
