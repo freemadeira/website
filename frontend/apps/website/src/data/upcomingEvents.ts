3b2fbfdc354f82bf38b94b36985e32fec619088b
Henrique Albuquerque <18596542+henrialb@users.noreply.github.com>
2026-08-08 00:11:03 +0100

    Update events

--- frontend/apps/website/src/data/upcomingEvents.ts
+++ frontend/apps/website/src/data/upcomingEvents.ts
@@ -11,24 +11,6 @@ export interface Event {
 
 export const events: Event[] = [
   {
-    date: 'July 1, 2026, 18:30',
-    title: 'Correr um Nó de Bitcoin',
-    description:
-      'Correr um nó de Bitcoin é uma das formas mais eficazes de contribuir para a rede. Neste workshop, vamos mostrar-te como configurar e correr o teu próprio nó de Bitcoin.',
-    tags: ['Education', 'Workshop', 'Online'],
-    imgSrc: '/images/events/workshop-intro.jpg',
-    href: 'https://docs.google.com/forms/d/e/1FAIpQLSf29qvdeanTjcjB10FQRB7YhYkCVYmK2LKOyfAwVKYOD8mtRQ/viewform',
-  },
-  {
-    date: 'July 29, 2026, 18:30',
-    title: 'FREE Madeira Bitcoin Meetup',
-    description:
-      'Every last Wednesday of the month, FREE Madeira organises a Bitcoin meetup, bringing the community together. Know more and sign up.',
-    tags: ['Meetup', 'In-person'],
-    imgSrc: '/images/events/meetup.jpg',
-    href: 'https://www.satlantis.io/events/1696/FREE-Madeira-Bitcoin-Meetup',
-  },
-  {
     date: 'August 26, 2026, 18:30',
     title: 'FREE Madeira Bitcoin Meetup',
     description:
@@ -46,24 +28,24 @@ export const events: Event[] = [
     imgSrc: '/images/events/meetup.jpg',
     href: 'https://www.satlantis.io/events/1698/FREE-Madeira-Bitcoin-Meetup',
   },
-  // {
-  //   date: 'October 28, 2026, 18:30',
-  //   title: 'FREE Madeira Bitcoin Meetup',
-  //   description:
-  //     'Every last Wednesday of the month, FREE Madeira organises a Bitcoin meetup, bringing the community together. Know more and sign up.',
-  //   tags: ['Meetup', 'In-person'],
-  //   imgSrc: '/images/events/meetup.jpg',
-  //   href: 'https://www.satlantis.io/events/1699/FREE-Madeira-Bitcoin-Meetup',
-  // },
-  // {
-  //   date: 'November 25, 2026, 18:30',
-  //   title: 'FREE Madeira Bitcoin Meetup',
-  //   description:
-  //     'Every last Wednesday of the month, FREE Madeira organises a Bitcoin meetup, bringing the community together. Know more and sign up.',
-  //   tags: ['Meetup', 'In-person'],
-  //   imgSrc: '/images/events/meetup.jpg',
-  //   href: 'https://www.satlantis.io/events/1700/FREE-Madeira-Bitcoin-Meetup',
-  // },
+  {
+    date: 'October 28, 2026, 18:30',
+    title: 'FREE Madeira Bitcoin Meetup',
+    description:
+      'Every last Wednesday of the month, FREE Madeira organises a Bitcoin meetup, bringing the community together. Know more and sign up.',
+    tags: ['Meetup', 'In-person'],
+    imgSrc: '/images/events/meetup.jpg',
+    href: 'https://www.satlantis.io/events/1699/FREE-Madeira-Bitcoin-Meetup',
+  },
+  {
+    date: 'November 25, 2026, 18:30',
+    title: 'FREE Madeira Bitcoin Meetup',
+    description:
+      'Every last Wednesday of the month, FREE Madeira organises a Bitcoin meetup, bringing the community together. Know more and sign up.',
+    tags: ['Meetup', 'In-person'],
+    imgSrc: '/images/events/meetup.jpg',
+    href: 'https://www.satlantis.io/events/1700/FREE-Madeira-Bitcoin-Meetup',
+  },
   // {
   //   date: 'December 30, 2026, 18:30',
   //   title: 'FREE Madeira Bitcoin Meetup',
